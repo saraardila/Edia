645ddfc5888f88f5cf95cd6c30bbd2344ffd86b4
saraardila <saraardiladam@gmail.com>
2021-04-18 12:43:23 +0200

    Second Commmit

--- app/src/main/java/com/example/edia/MainActivity.java
+++ app/src/main/java/com/example/edia/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.edia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Log.d("tag","esto es una prueba");
 
     }
 }
